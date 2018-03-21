c05821b8a2e41926c60781d406eb91dab157969a
Dyah Ayu Mustikaning Tyas <dyahayumt@Dyahs-MacBook-Air.local>
2018-03-21 17:07:56 +0700

    restructure

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-var express           = require('express');
-var path              = require('path');
-var favicon           = require('serve-favicon');
-var logger            = require('morgan');
-var cookieParser      = require('cookie-parser');
-var bodyParser        = require('body-parser');
-var expressValidator  = require('express-validator');
-var index             = require('./routes/index');
-var users             = require('./routes/users');
-var app               = express();
-var flash             = require('connect-flash');
-var crypto            = require('crypto');
-var passport          = require('passport');
-var passportLocal     = require('passport-local').Strategy;
-var nodemailer        = require('nodemailer');
-var async             = require('async');
-var moment            = require('moment');
-var session           = require('express-session');
-var Store             = require('express-session').Store;
-var BetterMemoryStore = require('session-memory-store')(session);
-
-
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "wonderlabs",
-  database: "student_info"
-});
+const express           = require('express');
+const path              = require('path');
+const favicon           = require('serve-favicon');
+const logger            = require('morgan');
+const cookieParser      = require('cookie-parser');
+const bodyParser        = require('body-parser');
+const index             = require('./routes/index');
+const users             = require('./routes/users');
+const con               = require('./routes/dbconfig');
+const app               = express();
+const flash             = require('connect-flash');
+const crypto            = require('crypto');
+const passport          = require('passport');
+const passportLocal     = require('passport-local').Strategy;
+const async             = require('async');
+app.locals.moment       = require('moment');
+const moment            = require('moment');
+const session           = require('express-session');
+const Store             = require('express-session').Store;
+const BetterMemoryStore = require('session-memory-store')(session);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -236,36 +226,12 @@ app.post("/login", passport.authenticate('local', {
 }), function(req, res, info){
   res.render('login',{'message' :req.flash('message')});
 });
-
-function formatDateTime(date, type) {
-  var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [day, month, year].join('/');
-}
-
-function formatDate(date, type) {
-  var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
-
-  return [day, month, year].join('-');
-  } 
-
+  
 function getStudentGender(rows, studentGender){
   if(studentGender === 'M'){
-    gender = 'Male';
+    gender = 'M';
   } else {
-    gender = 'Female';
+    gender = 'F';
   }
   return gender;
 }
@@ -286,199 +252,15 @@ app.get('/students', isAuthenticated, function(req, res) {
       res.status(500).json({"status_code": 500,"status_message": "internal server error"});
     } else {
       console.log(rows);
-
-      // Loop check on each row
-      for (var i = 0; i < rows.length; i++) {
-        var dateOfBirth = formatDate(rows[i].date_of_birth);
-        var dateTime = formatDate(rows[i].date_time);
-
-        // Create an object to save current row's data
-        var student = {
-          'student_id':rows[i].student_id,
-          'first_name':rows[i].first_name,
-          'middle_name':rows[i].middle_name,
-          'last_name':rows[i].last_name,
-          'gender': rows[i].gender,
-          'place_of_birth':rows[i].place_of_birth,
-          'date_of_birth':formatDateTime(rows[i].date_of_birth),
-          'phone_number': rows[i].phone_number,
-          'email_address': rows[i].email_address,
-          'date_time': formatDateTime(rows[i].date_time)
-        }
-        // Add object into array
-        studentList.push(student);
-    }
-    // Render index.pug page using array 
-    res.render('index.pug', {title: 'Student List', data: studentList});
+    res.render('index.pug', {title: 'Student List', data: rows});
     }
   })
 });
 
-
-app.get('/input', function (req, res) {
-    res.render('input');
-});
-
-function dobval (){
-  var date_of_birth = req.body.date_of_birth;
-  var today = formatDate( new Date ());
-
-  if (date_of_birth >= today){
-    console.log("Data is invalid");
-    return false;
-  } else {
-    return true;
-  }
-}
-
- //write student details
-app.post('/input', function (req, res) {
-  // this is where you handle the POST request.
-  //if (dobval == true)
-  var createStudent = {
-   student_id: req.body.student_id,
-   first_name: req.body.first_name,
-   middle_name: req.body.middle_name,
-   last_name: req.body.last_name,
-   gender: req.body.radio,
-   place_of_birth: req.body.place_of_birth,
-   date_of_birth: req.body.date_of_birth,
-   phone_number: req.body.phone_number,
-   email_address: req.body.email_address,
-   date_time: req.body.date_time
-  }
-  console.log(createStudent);
-  
-  con.query('INSERT INTO student SET ?', createStudent, function (error, results, fields) {
-    if (error) throw error;
-    console.log("1 record inserted");
-		res.redirect('/students');
-  });
-})
-
-app.get('/students/:id', function(req, res) {
-	con.query('SELECT * FROM student WHERE student_id = ?', [req.params.id], function(err, rows, fields) {
-		if(err) throw err
-    else console.log(rows);
-		
-		// if user not found
-		if (rows.length <= 0) {
-				// req.flash('error', 'Student not found with id = ' + req.params.id)
-				res.redirect('/students')
-		}
-		else { // if user found
-				// render to views/index.pug template file
-				res.render('edit', {
-            title: 'Edit Student', 
-            student_id: rows[0].student_id,
-				    first_name: rows[0].first_name,
-				    middle_name: rows[0].middle_name,
-            last_name: rows[0].last_name,
-            gender: rows[0].gender,
-				    place_of_birth: rows[0].place_of_birth,
-            date_of_birth: formatDateTime(rows[0].date_of_birth),
-            phone_number: rows[0].phone_number,
-            email_address: rows[0].email_address,
-            date_time: formatDateTime(rows[0].date_time),
-            sOldId: rows[0].student_id
-        })
-		}            
-	});
-});
-
-app.post('/updated-student', function(req, res) {
-  var student_id = req.body.student_id;
-  var first_name = req.body.first_name;
-  var middle_name = req.body.middle_name;
-  var last_name = req.body.last_name;
-  var gender= req.body.radio;
-  var place_of_birth = req.body.place_of_birth;
-  var date_of_birth = req.body.date_of_birth;
-  var phone_number = req.body.phone_number;
-  var email_address = req.body.email_address;
-  var date_time = req.body.date_time;
-  var studentOldId = req.body.oldId;
-  console.log(student_id+' '+first_name+' '+middle_name+' '+last_name+' '+gender+' '+place_of_birth+' '+date_of_birth+' '+phone_number+' '+email_address +' '+date_time+' '+studentOldId);
-  var postData  = {student_id: student_id, first_name: first_name, middle_name: middle_name, last_name: last_name, gender: gender, date_of_birth: date_of_birth, phone_number: phone_number, date_time:date_time, email_address: email_address};
-
-  if(studentOldId !== undefined && studentOldId !== '') {
-      con.query('UPDATE student SET student_id = ?, first_name = ?, middle_name = ?, last_name = ?, gender = ? ,place_of_birth = ?, date_of_birth = ?, phone_number = ?, email_address = ?, date_time = ? WHERE student_id = ?', [student_id, first_name, middle_name, last_name, gender, place_of_birth, date_of_birth, phone_number, email_address, date_time, studentOldId], function (error, results, fields) {
-          if (error) throw error;
-          res.redirect('/students');
-      });
-  } else {
-      con.query('INSERT INTO student SET ?', postData, function (error, results, fields) {
-          if (error) throw error;
-          res.redirect('/students');
-      });
-  }
-});
-
-app.post('/delete/:id', function (req, res) { 
-    con.query('DELETE FROM student WHERE student_id = ?', [req.params.id], function(err, result) {
-      if(err) throw err
-      res.redirect('/students');
-    });
-  });
-
-  function transpose(original) {
-    var copy = [];
-    for (var i = 0; i < original.length; ++i) {
-        for (var j = 0; j < original[i].length; ++j) {
-            // skip undefined values to preserve sparse array
-            if (original[i][j] === undefined) continue;
-            // create row if it doesn't exist yet
-            if (copy[j] === undefined) copy[j] = [];
-            // swap the x and y coords for the copy
-            copy[j][i] = original[i][j];
-        }
-    }
-    return copy;
-    }
-  
-  app.get('/students/statistics/:year', function(req, res)  {
-    var getMonth = []; getFreq = []; month = []; freq = [] ; temp_MonthFreq=[]; trans_month=[]; getGender = []; getFreqGen = []; temp_genderFreq=[]; trans_gend=[];
-    getMonth.push('month', 'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER');
-    getFreq.push('freq', 0,0,0,0,0,0,0,0,0,0,0,0);
-    con.query('SELECT month(date_time) as month, count(*) as freq FROM student WHERE year(date_time)='+[req.params.year]+' group by month(date_time)', function(err, rows, fields) {
-      console.log(rows);
-      if (err) {
-          console.log(err);
-        } else {
-          // getFreq.push('freq')
-          for (var j = 0 ; j < rows.length ; j++) {
-            var month = rows[j].month;
-            getFreq.fill(rows[j].freq, month, month+1);       
-          }
-          temp_MonthFreq.push(getMonth,getFreq)
-        }
-        var trans_month = transpose(temp_MonthFreq);  
-        console.log(trans_month);
-  
-      con.query('select * from freq_gen', function(err, rows, fields) {
-        if (err) {
-          console.log(err)
-        } else {
-          getGender.push('gender')
-          getFreqGen.push('freq_gender')
-          for (var j = 0 ; j < rows.length ; j++) {
-            if (rows[j].gender === 'F') {
-              getGender.push('F')
-            } else {
-              getGender.push('M')
-            }
-            getFreqGen.push(rows[j].freq_gender)       
-          }
-          temp_genderFreq.push(getGender,getFreqGen)
-        }
-        var trans_gend = transpose(temp_genderFreq);  
-        console.log(trans_gend);
-        res.render('statistics',{obj1: JSON.stringify(trans_month), obj2: JSON.stringify(trans_gend)});
-      })  
-    })  
-  });  
-
 app.post('/search', function(req, res) {
+  // var sql = "SELECT * FROM ?? WHERE ?? = ?";
+  // var inserts = ['users', 'id', userId];
+  // sql = mysql.format(sql, inserts);
   var studentFilter = [];
   var keyword = req.body.keyword;
   var opt = req.body.opt;
@@ -486,7 +268,7 @@ app.post('/search', function(req, res) {
   var mysqlquery;
   var sort= req.body.sort;
 
-  con.query('SELECT * FROM student WHERE '+opt+' LIKE  \'%' + keyword +'%\' ORDER BY '+opt+' '+sortBy+'', function(err, rows, fields) {
+  con.query('SELECT * FROM student WHERE ?? LIKE  \'?%' + keyword+'?%\' ORDER BY ?? ?? '+sort[opt, sortBy, opt], function(err, rows, fields) {
     if (err) {
       res.status(500).json({"status_code": 500,"status_message": "internal server error"});
     } else {
@@ -501,10 +283,10 @@ app.post('/search', function(req, res) {
           'last_name':rows[i].last_name,
           'gender': rows[i].gender,
           'place_of_birth':rows[i].place_of_birth,
-          'date_of_birth':formatDateTime(rows[i].date_of_birth),
+          'date_of_birth':moment(rows[i].date_of_birth).format('YYYY-MM-DD'),
           'phone_number': rows[i].phone_number,
           'email_address': rows[i].email_address,
-          'date_time': formatDateTime(rows[i].date_time)
+          'date_time': moment(rows[i].date_time).format('YYYY-MM-DD')
         }
        // Add object into array
         studentFilter.push(student);
@@ -516,6 +298,7 @@ app.post('/search', function(req, res) {
 );
 });
 
+app.use('/', isAuthenticated, index);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -535,3 +318,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+//server.listen(config.server.port);
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,181 @@
-const express = require('express');
-const router = express.Router();
-const mysql = require('mysql');
-
- var con = mysql.createConnection({
-   host: "localhost",
-   user: "root",
-   password: "wonderlabs",
-   database: "student_info"
- });
+const express           = require('express');
+const path              = require('path');
+const favicon           = require('serve-favicon');
+const logger            = require('morgan');
+const cookieParser      = require('cookie-parser');
+const bodyParser        = require('body-parser');
+const con               = require('../routes/dbconfig');
+const expressValidator  = require('express-validator');
+const flash             = require('connect-flash');
+const crypto            = require('crypto');
+const passport          = require('passport');
+const passportLocal     = require('passport-local').Strategy;
+const async             = require('async');
+const moment            = require('moment');
+const session           = require('express-session');
+const Store             = require('express-session').Store;
+const BetterMemoryStore = require('session-memory-store')(session);
+const router            = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   con.connect(function(err) {
+   conn.connect(function(err) {
      if (err) throw err;
      console.log("Connected!");
    });
   
    res.send('express');    
  });
+
+router.get('/input', function (req, res) {
+    res.render('input');
+});
+
+ //write student details
+router.post('/input', function (req, res) {
+  // this is where you handle the POST request.
+  //if (dobval == true)
+  var createStudent = {
+   student_id: req.body.student_id,
+   first_name: req.body.first_name,
+   middle_name: req.body.middle_name,
+   last_name: req.body.last_name,
+   gender: req.body.radio,
+   place_of_birth: req.body.place_of_birth,
+   date_of_birth: req.body.date_of_birth,
+   phone_number: req.body.phone_number,
+   email_address: req.body.email_address,
+   date_time: req.body.date_time
+  }
+  console.log(createStudent);
+  
+  con.query('INSERT INTO student SET ?', createStudent, function (error, results, fields) {
+    if (error) throw error;
+    console.log("1 record inserted");
+		res.redirect('/students');
+  });
+})
+
+router.get('/students/:id', function(req, res) {
+	con.query('SELECT * FROM student WHERE student_id = ?', [req.params.id], function(err, rows, fields) {
+		if(err) throw err
+    else console.log(rows);
+		
+		// if user not found
+		if (rows.length <= 0) {
+				// req.flash('error', 'Student not found with id = ' + req.params.id)
+				res.redirect('/students')
+		}
+		else { // if user found
+				// render to views/index.pug template file
+				res.render('edit', {
+            title: 'Edit Student', 
+            student_id: rows[0].student_id,
+				    first_name: rows[0].first_name,
+				    middle_name: rows[0].middle_name,
+            last_name: rows[0].last_name,
+            gender: rows[0].gender,
+				    place_of_birth: rows[0].place_of_birth,
+            date_of_birth: formatDateTime(rows[0].date_of_birth),
+            phone_number: rows[0].phone_number,
+            email_address: rows[0].email_address,
+            date_time: formatDateTime(rows[0].date_time),
+            sOldId: rows[0].student_id
+        })
+		}            
+	});
+});
+
+router.post('/updated-student', function(req, res) {
+  var student_id = req.body.student_id;
+  var first_name = req.body.first_name;
+  var middle_name = req.body.middle_name;
+  var last_name = req.body.last_name;
+  var gender= req.body.radio;
+  var place_of_birth = req.body.place_of_birth;
+  var date_of_birth = req.body.date_of_birth;
+  var phone_number = req.body.phone_number;
+  var email_address = req.body.email_address;
+  var date_time = req.body.date_time;
+  var studentOldId = req.body.oldId;
+  console.log(student_id+' '+first_name+' '+middle_name+' '+last_name+' '+gender+' '+place_of_birth+' '+date_of_birth+' '+phone_number+' '+email_address +' '+date_time+' '+studentOldId);
+  var postData  = {student_id: student_id, first_name: first_name, middle_name: middle_name, last_name: last_name, gender: gender, date_of_birth: date_of_birth, phone_number: phone_number, date_time:date_time, email_address: email_address};
+
+  if(studentOldId !== undefined && studentOldId !== '') {
+      con.query('UPDATE student SET student_id = ?, first_name = ?, middle_name = ?, last_name = ?, gender = ? ,place_of_birth = ?, date_of_birth = ?, phone_number = ?, email_address = ?, date_time = ? WHERE student_id = ?', [student_id, first_name, middle_name, last_name, gender, place_of_birth, date_of_birth, phone_number, email_address, date_time, studentOldId], function (error, results, fields) {
+          if (error) throw error;
+          res.redirect('/students');
+      });
+  } else {
+      con.query('INSERT INTO student SET ?', postData, function (error, results, fields) {
+          if (error) throw error;
+          res.redirect('/students');
+      });
+  }
+});
+
+router.post('/delete/:id', function (req, res) { 
+    con.query('DELETE FROM student WHERE student_id = ?', [req.params.id], function(err, result) {
+      if(err) throw err
+      res.redirect('/students');
+    });
+  });
+
+  function transpose(original) {
+    var copy = [];
+    for (var i = 0; i < original.length; ++i) {
+        for (var j = 0; j < original[i].length; ++j) {
+            // skip undefined values to preserve sparse array
+            if (original[i][j] === undefined) continue;
+            // create row if it doesn't exist yet
+            if (copy[j] === undefined) copy[j] = [];
+            // swap the x and y coords for the copy
+            copy[j][i] = original[i][j];
+        }
+    }
+    return copy;
+    }
+  
+  router.get('/students/statistics/:year', function(req, res)  {
+    var getMonth = []; getFreq = []; month = []; freq = [] ; temp_MonthFreq=[]; trans_month=[]; getGender = []; getFreqGen = []; temp_genderFreq=[]; trans_gend=[];
+    getMonth.push('month', 'JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER');
+    getFreq.push('freq', 0,0,0,0,0,0,0,0,0,0,0,0);
+    con.query('SELECT month(date_time) as month, count(*) as freq FROM student WHERE year(date_time)='+[req.params.year]+' group by month(date_time)', function(err, rows, fields) {
+      console.log(rows);
+      if (err) {
+          console.log(err);
+        } else {
+          // getFreq.push('freq')
+          for (var j = 0 ; j < rows.length ; j++) {
+            var month = rows[j].month;
+            getFreq.fill(rows[j].freq, month, month+1);       
+          }
+          temp_MonthFreq.push(getMonth,getFreq)
+        }
+        var trans_month = transpose(temp_MonthFreq);  
+        console.log(trans_month);
+  
+      con.query('select * from freq_gen', function(err, rows, fields) {
+        if (err) {
+          console.log(err)
+        } else {
+          getGender.push('gender')
+          getFreqGen.push('freq_gender')
+          for (var j = 0 ; j < rows.length ; j++) {
+            if (rows[j].gender === 'F') {
+              getGender.push('F')
+            } else {
+              getGender.push('M')
+            }
+            getFreqGen.push(rows[j].freq_gender)       
+          }
+          temp_genderFreq.push(getGender,getFreqGen)
+        }
+        var trans_gend = transpose(temp_genderFreq);  
+        console.log(trans_gend);
+        res.render('statistics',{obj1: JSON.stringify(trans_month), obj2: JSON.stringify(trans_gend)});
+      })  
+    })  
+  });  
  
 module.exports = router;
